ddb2c22f76b7d2cba8c551fa574a97a338176691
kattak2k <kkiran.ror@gmail.com>
2017-04-29 05:42:09 +0530

    added annotate gem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  message_id :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ApplicationRecord
   belongs_to :message
   belongs_to :user
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 class Message < ApplicationRecord
     belongs_to :user
     has_many :comments
