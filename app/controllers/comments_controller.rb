b10465ae6a22e96c324801305a848f9d64ebe097
kattak2k <kkiran.ror@gmail.com>
2017-04-29 05:37:12 +0530

    minor bug fixes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,50 +1,56 @@
 class CommentsController < ApplicationController
-
-before_action :find_message
-before_action :find_comment, only: [:edit, :update, :destroy]
-# def new    
-# end
-
-def create
-       #POST   /messages/:message_id/comments(.:format)          comments#create
-  #@message = Message.find(params[:message_id])
-  @comment = @message.comments.create(comment_params)
-  @comment.user_id = current_user.id
-
-  if @comment.save
-      redirect_to message_path(@message)
-  else
-      render 'new'    
-  end    
-end
-
-def edit
-end
-
-def update
-    if @comment.update
-       redirect_to message_path(@mssage)
-    else   
-       render 'edit'
+    before_action :authenticate_user!
+    before_action :find_message#, only: [:show,:edit, :update, :destroy]
+    before_action :find_comment, only: [:edit, :update, :destroy]
+    def new    
     end
-end
 
-def destroy
-    @comment.destroy
-    redirect_to message_path(@mssage)
-end
-private
+    def create
+        #POST   /messages/:message_id/comments(.:format)          comments#create
+    #@message = Message.find(params[:message_id])
+    #@comment = @message.comments.create(comment_params)
+    @comment = @message.comments.new(comment_params)
+    @comment.user_id = current_user.id
 
-def find_message
-  @message = Message.find(params[:message_id])
-end
+     unless @comment.content.blank?
+        if @comment.save
+           redirect_to message_path(@message)
+        else
+           render 'new'    
+        end
+     else
+       redirect_to message_path(@message)    
+     end
+    end
 
-def find_comment
-  @comment = @message.comments.find(params[:message_id])
-end
+    def edit
+    end
 
-def comment_params
-    params.require(:comment).permit(:content,:user_id,:message_id)
-end
+    def update
+        if @comment.update
+        redirect_to message_path(@message)
+        else   
+        render 'edit'
+        end
+    end
+
+    def destroy
+        @comment.destroy
+        redirect_to message_path(@message)
+    end
+
+    private
+
+    def find_message
+        @message = Message.find(params[:message_id])
+    end
+
+    def find_comment
+        @comment = @message.comments.find(params[:id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:content,:user_id,:message_id)
+    end
 
 end
